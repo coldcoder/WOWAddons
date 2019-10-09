245c0cf015b282b2fcb6e1db305dc39582f2e5ef
ColdCoder <gyzhang1618@hotmail.com>
2019-10-09 17:52:46 +0800

    update

---
+++ AddOns/GTFO/GTFO_Mobs.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------
+-- GTFO_Mobs.lua 
+--------------------------------------------------------------------------
+--[[
+GTFO Mob List
+Author: Zensunim of Malygos
+
+Sample:
+
+GTFO.MobID["12345"] = {
+	--desc = "Mob Name - Boss - Instance";
+	sound = 1;
+	tankSound = 1;
+	damageOnly = true;
+};
+
+]]--
+
+GTFO.MobID["7915"] = {
+	--desc = "Walking Bomb - Gnomeregan";
+	sound = 3;
+	tankSound = 2;
+	trivialLevel = 50;
+};
+
